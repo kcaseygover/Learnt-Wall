8a4f4c04f6e68aa51b7a08494e9e0db9f8cdf920
Karen Casey-Gover <karen.casey@hotmail.com>
2016-10-30 16:50:01 +0000

    added seed data to categories

--- db/seeds/categories.js
+++ db/seeds/categories.js
@@ -2,15 +2,17 @@ const uuid = require('node-uuid');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-
-  return Promise.all([
+  return knex('categories').del()
+  .then(function () {
+    return Promise.all([
         // Inserts seed entries
-        knex('categories').insert({id: uuid.v4, name: ''}),
-        knex('categories').insert({id: uuid.v4, name: ''}),
-        knex('categories').insert({id: uuid.v4, name: ''}),
-        knex('categories').insert({id: uuid.v4, name: ''}),
-        knex('categories').insert({id: uuid.v4, name: ''}),
-        knex('categories').insert({id: uuid.v4, name: ''}),
+        knex('categories').insert({id: uuid.v4(), name: 'Javascript'}),
+        knex('categories').insert({id: uuid.v4(), name: 'Databases'}),
+        knex('categories').insert({id: uuid.v4(), name: 'HTML/CSS'}),
+        knex('categories').insert({id: uuid.v4(), name: 'AJAX'}),
+        knex('categories').insert({id: uuid.v4(), name: 'jQuery'}),
+        knex('categories').insert({id: uuid.v4(), name: 'node'}),
         ]);
 
-};
+  });
+}
