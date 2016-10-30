a0196e1f5fc6ef264298bc51ebe4fc390613080c
laurensmalld <lauren.dentremont@gmail.com>
2016-10-30 16:36:22 +0000

    added bootstrap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-$(() => {
+/*$(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -7,4 +7,102 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+});*/
+
+$(document).ready(function () {
+
+/*    var users = [
+    { user_id: 'Lauren',
+    password: '12345'},
+    { user_id: 'Karen',
+    password: '67890'}
+    ];
+
+    var resources = [
+      {user: { user_id: 'Lauren',
+      resource_id: 'url1',
+      url: 'www.lighthouselabs.com',
+      title: 'lighthouse Labs',
+      description: '8 week web development bootcamp info',
+      category: 'education'}},
+      {user: { user_id: 'Karen'
+      resource_id: 'url2',
+      url: 'www.voltaeffect.com',
+      title: 'Volta Labs',
+      description: 'coworking space info',
+      category: 'coworking'}}
+    ];*/
+
+      //functions to create the resource template then move the db info into it
+    function renderResources(resourcesList) {
+      $('.resource-feed').empty();
+
+       // var sortResources = resourcesList.sort(function (a, b) {
+        //  return a.created_at < b.created_at;
+      //  });
+      resourcesList.forEach(function (resource) {
+        $('.resource-feed').append(createResourceElement(resource));
+      });
+    };
+
+      //renderResources(resources);
+
+    function createResourceElement(obj) {
+      var $resource =
+      `<article>
+      <header class="resource-head">
+      <a href="${obj.user.url}"><h3>${obj.user.title}</h3></a>
+      <main>
+      <p>${obj.user.description}</p>
+      </main>
+      <footer class="resource-foot">
+      <p>${obj.user.category}</p>
+      <img class="icons" src="/images/reply-action.png">
+      <img class="icons" src="/images/retweet-action.png">
+      <img class="icons" src="/images/like-action.png">
+      </footer>
+      </article>`
+      return $resource;
+    };
+
+
+        //load resources from db
+        function loadResources() {
+          $.ajax({
+            url: 'user.id/resources',
+            method: 'GET',
+            success: function (moreResources) {
+              console.log('Success: ', moreResources);
+              renderResources(moreResources)
+            }
+          });
+        };
+
+        loadResources();
+
+          //new resource form functionality with logic/error msgs for empty
+          // also clears the textarea after successful submission
+  $('#add-new').submit(function (ev) {
+            ev.preventDefault();
+            var $postData = $(this).serialize();
+            var $textVal = $(this).find('textarea').val();
+            if ($textVal === "") {
+              $('#flash').append("Your new resource must have a URL, title AND description!")
+            } else {
+              $.ajax({
+                url:'/resources',
+                method: 'POST',
+                data: $postData,
+                success: function (result) {
+                  console.log('Success: ', result);
+                  loadResources()
+                }
+              }); } $('textarea').val('');
+            });
+    $('.new-resource').hide();
+        $('#add-button').click(function () {
+      $('.new-resource').slideToggle( "fast", function () {
+        $('.exampleInputEmail1').focus();
+      });
+    });
 });
